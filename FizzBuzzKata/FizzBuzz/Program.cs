450e9df747f1f7cf7378421d6dfc311765ff8a00
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2022-01-17 19:45:48 +0000

    Fizz Buzz Kata

---
+++ FizzBuzzKata/FizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FizzBuzz;
+
+Console.WriteLine("Hello, World!");
+
+
+for (int i = 1; i < 100; i++)
+{
+    Console.WriteLine(FizzBuzzLogic.PrintOption(i));
+}
+
+
---
+++ FizzBuzzKata/FizzBuzzTest/PrintTest.cs
@@ -0,0 +1,27 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTest
+{
+    public class PrintTest
+    {
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        public void return_fizz_buzz_fizzbuzz(int value, string rtnValue)
+        {
+            
+            Assert.AreEqual(rtnValue, FizzBuzzLogic.PrintOption(value));
+ 
+        }
+
+    }
+}
